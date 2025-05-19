560bb26635db95a663e91b03f64b26587b274e31
FAIT ltd <126212038+Faitltd@users.noreply.github.com>
2025-05-18 23:09:00 -0600

    Add aliases and test config

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,23 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { resolve } from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
 
   kit: {
-    adapter: adapter()
+    adapter: adapter(),
+    alias: {
+      $lib: resolve('./src/lib'),
+      $components: resolve('./src/lib/components'),
+      $stores: resolve('./src/lib/stores'),
+      $utils: resolve('./src/lib/utils'),
+      $firebase: resolve('./src/lib/firebase'),
+      $types: resolve('./src/lib/types'),
+      $services: resolve('./src/lib/services'),
+      $constants: resolve('./src/lib/constants')
+    }
   }
 };
 
