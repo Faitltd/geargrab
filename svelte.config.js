a0fe782957e13450892b18b1983edec8e637a6da
FAIT ltd <126212038+Faitltd@users.noreply.github.com>
2025-05-18 19:21:16 -0600

    fix build docs and add cypress config

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,24 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const alias = {
+  $lib: 'src/lib',
+  $components: 'src/lib/components',
+  $stores: 'src/lib/stores',
+  $utils: 'src/lib/utils',
+  $firebase: 'src/lib/firebase',
+  $types: 'src/lib/types',
+  $services: 'src/lib/services',
+  $constants: 'src/lib/constants'
+};
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
 
   kit: {
-    adapter: adapter()
+    adapter: adapter(),
+    alias
   }
 };
 
